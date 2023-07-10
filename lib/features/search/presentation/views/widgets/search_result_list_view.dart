81153cb376b0b56a52d15ac612da729e675beb7d
Mostafa Rabie Alaasr <73349514+MostafaEla3sr@users.noreply.github.com>
2023-07-10 16:37:31 +0200

    create search view

--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'best_seller_list_view_item.dart';
+import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({Key? key}) : super(key: key);
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        physics: const NeverScrollableScrollPhysics(),
         padding: EdgeInsets.zero,
         itemCount: 10,
         itemBuilder: (context, index) {
