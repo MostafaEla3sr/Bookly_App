8275057a3d987bb24fbc0801157018c179db7966
Mostafa Rabie Alaasr <73349514+MostafaEla3sr@users.noreply.github.com>
2023-08-15 18:33:50 +0200

    fetch similar books done

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,8 +1,25 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view_body.dart';
 import 'package:flutter/material.dart';
-class BookDetailsView extends StatelessWidget {
-  const BookDetailsView({Key? key}) : super(key: key);
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../manager/similar_books_cubit/similar_books_cubit.dart';
+class BookDetailsView extends StatefulWidget {
+  const BookDetailsView({Key? key, required this.bookModel}) : super(key: key);
+final BookModel bookModel;
+  @override
+  State<BookDetailsView> createState() => _BookDetailsViewState();
+}
+
+class _BookDetailsViewState extends State<BookDetailsView> {
+
+  @override
+  void initState() {
+    BlocProvider.of<SimilarBooksCubit>(context).fetchSimilarBooks(
+      category:widget.bookModel.volumeInfo.categories![0],
+    );
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'custom_book_item.dart';
 
 class SimilarBooksListView extends StatelessWidget {
@@ -6,20 +10,35 @@ class SimilarBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.14,
-      child: ListView.builder(
-          itemCount: 7,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5),
-              child: CustomBookImage(
-                imageUrl: 'https://m.media-amazon.com/images/I/71HbYElfY0L._AC_UF1000,1000_QL80_.jpg',
-
-              ),
-            );
-          }),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height * 0.14,
+            child: ListView.builder(
+                itemCount: 7,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5),
+                    child: CustomBookImage(
+                      imageUrl: 'https://m.media-amazon.com/images/I/71HbYElfY0L._AC_UF1000,1000_QL80_.jpg',
+          
+                    ),
+                  );
+                }),
+          );
+        }
+        else if (state is SimilarBooksFailure){
+          return CustomErrorWidget(errMessage: state.errMessage);
+        }
+        else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
